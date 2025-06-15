6c98e7647c94e6bc2c2f219cb0700713e605fa9f
Sachin Lakshitha <niroshasulochini@gmail.com>
2025-06-16 03:00:45 +0530

    Add files via upload

---
+++ liqour-sales-website/index.php
@@ -0,0 +1,49 @@
+<?php include('header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Liquor Website</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <section class="nav-bar">
+    <div class="logo-container">
+      <img src="src/045yr3xhag3tsp47jt0r6xnk80_image.png" alt="Logo">
+    </div>
+
+    <div class="nav-options-container nav-options-font">
+      <a href=""><div class="nav-option">LIQUOR</div></a>
+      <a href=""><div class="nav-option">FEATURED</div></a>
+      <a href=""><div class="nav-option">CATEGORIES</div></a>
+      <a href=""><div class="nav-option">WINES</div></a>
+    </div>
+
+    <div class="profile-search-cart">
+      <div><a href=""><img class="search icon" src="src/search-interface-symbol.png" alt="Search"></a></div>
+      <div><a href=""><img class="profile icon" src="src/profile-user.png" alt="Profile"></a></div>
+      <div><a href=""><img class="cart icon" src="src/grocery-store.png" alt="Cart"></a></div>
+    </div>
+  </section>
+
+  <section class="feedback-socials">
+    <div class="social-media-links">
+      <a href=""><img class="icon" src="src/facebook-app-symbol.png" alt="Facebook"></a>
+      <a href=""><img class="icon" src="src/instagram.png" alt="Instagram"></a>
+      <a href=""><img class="icon" src="src/twitter.png" alt="X"></a>
+      <a href=""><img class="icon" src="src/tik-tok.png" alt="TikTok"></a>
+    </div>
+    <div class="feedback-text">
+      <p>Take a look at our reviews!</p>
+    </div>
+    <DIV>
+       <a href=""><div><P>News</P></div></a>
+        <a href=""><div><p>Contact us</p></div></a>
+        <a href=""><div><p>Delivery</p></div></a>
+    </DIV>
+  </section>
+
+  <?php include('slider.html'); ?>
+  </body>
+</html>
