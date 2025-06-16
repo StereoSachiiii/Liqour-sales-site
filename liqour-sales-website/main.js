b8a240e2cee968b67af93a92c17e05b1ea6d6ed6
Your Name <niroshasulochini@gmail.com>
2025-06-17 04:06:28 +0530

    css /pictures / and product using js objects
    i have no idea what im doing 0_0 changed some css made the new arrivals section
    fixed a bug where i had a body section included inside another php body

--- liqour-sales-website/main.js
+++ liqour-sales-website/main.js
@@ -42,4 +42,73 @@ function prevSlide() {
 prevBtn.addEventListener("click", prevSlide);
 nextBtn.addEventListener("click", nextSlide);
 
-setInterval(nextSlide,5000);
+setInterval(nextSlide, 8000);
+
+
+let cart = JSON.parse(localStorage.getItem('shoppingCart')) || {};
+const cartCountSpan = document.getElementById('cart-count');
+
+
+const newArrivals = document.querySelector(".new-arrivals");
+
+whatsNew.forEach((item) => {
+    
+    const newItem = document.createElement("div");
+    newItem.classList.add("new-item");
+
+   
+    const newItemImageContainer = document.createElement("div");
+    newItemImageContainer.classList.add("image-container");
+    newItemImageContainer.style.backgroundImage = `url(${item.image})`;
+    newItemImageContainer.style.backgroundSize = "cover";
+    newItemImageContainer.style.backgroundPosition = "center";
+
+   
+    const description = document.createElement("div");
+    description.classList.add("description");
+    description.innerText = item.name;
+
+
+
+  
+
+  
+    const priceAddToCartContainer = document.createElement("div");
+    priceAddToCartContainer.classList.add("price-add-to-cart");
+
+    const quantityInput = document.createElement("input");
+    quantityInput.setAttribute("type", "number");
+    quantityInput.setAttribute("min", "1");
+    quantityInput.setAttribute("value", "1");
+    quantityInput.classList.add("quantity-input");
+    
+
+  
+    const priceElement = document.createElement("span");
+    priceElement.classList.add("product-price");
+    priceElement.innerText = item.price; 
+
+    
+    const addToCartBtn = document.createElement("button");
+    addToCartBtn.classList.add("add-to-cart-btn");
+    addToCartBtn.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+
+
+    `; 
+
+    
+    priceAddToCartContainer.appendChild(priceElement);
+    priceAddToCartContainer.appendChild(quantityInput);
+    priceAddToCartContainer.appendChild(addToCartBtn);
+    
+
+   
+    newItem.appendChild(newItemImageContainer);
+    newItem.appendChild(description);
+    newItem.appendChild(priceAddToCartContainer); 
+
+    newArrivals.appendChild(newItem);
+});
---
+++ liqour-sales-website/product_data.js
@@ -0,0 +1,59 @@
+const whatsNew = [
+    {
+        name: 'Ceylon Spiced Arrack',
+        image: 'src/new/01.jpg',
+        price: 'Rs. 2,500.00' 
+    },
+    {
+        name: 'Tropical Gin Fusion',
+        image: 'src/new/02.jpg',
+        price: 'Rs. 3,200.00'
+    },
+    {
+        name: 'Limited Edition Irish Whiskey',
+        image: 'src/new/03.jpg',
+        price: 'Rs. 8,900.00'
+    },
+    {
+        name: 'Artisanal Craft Beer Selection',
+        image: 'src/new/04.jpg',
+        price: 'Rs. 1,800.00'
+    }
+];
+
+const bestSellers = [
+  {
+    name: 'Rockland Old Arrack',
+    image: '',
+  },
+  {
+    name: 'Black Label Blended Scotch',
+    image: '',
+  },
+  {
+    name: 'Absolut Vodka',
+    image: '',
+  },
+  {
+    name: 'Lion Lager Beer (6-pack)',
+    image: '',
+  },
+];
+const specialOffers = [
+  {
+    name: 'Wine & Cheese Pairing Deal',
+    image: '',
+  },
+  {
+    name: 'Vodka & Mixer Bundle (Save 15%)',
+    image: '',
+  },
+  {
+    name: 'Whiskey Tumbler Set (Free with purchase)',
+    image: '',
+  },
+  {
+    name: 'Flash Sale: Select Red Wines',
+    image: '',
+  },
+];
